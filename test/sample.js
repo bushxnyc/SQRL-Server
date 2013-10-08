d58f2ae8c4512b553e6da94b9c75d894083c61ed
Blake Arnold <arnold.blake@gmail.com>
2013-10-08 17:45:38 -0400

    Fixing test script

--- test/sample.js
+++ test/sample.js
@@ -6,7 +6,7 @@ var nonce = process.argv[2] | 'd0ee296585582b7b77d3fdc3a1f12bec';
 var msg = new Buffer(64);
 msg.write('https://sqrl.blakearnold.me/sqrl?' + nonce);
 
-var seed = process.argv[3] | '1234567890abcdefghij1234567890ab';
+var seed = '1234567890abcdefghij1234567890ab';
 
 // create hash update with seed and digest returns buffer
 var hash = crypto.createHash('sha256').update(seed).digest();
