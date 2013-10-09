952d383069a480afc77caf47e41899f92ac868ee
Blake Arnold <arnold.blake@gmail.com>
2013-10-09 07:59:20 -0400

    Fixing test script

--- test/sample.js
+++ test/sample.js
@@ -1,26 +1,24 @@
 // node sample d0ee296585582b7b77d3fdc3a1f12bec
+
 var crypto = require('crypto'),
     request = require('request'),
     ecc = require('../node_modules/ed25519/native');
 
-var nonce = 'd0ee296585582b7b77d3fdc3a1f12bec';
-var msg = new Buffer(64);
+var nonce = 'd0ee296585582b7b77d3fdc3a1f12bec',
+    msg = new Buffer(64);
 msg.write('sqrl.blakearnold.me/sqrl?' + nonce);
 
-var seed = '1234567890abcdefghij1234567890ab';
-
 // create hash update with seed and digest returns buffer
-var hash = crypto.createHash('sha256').update(seed).digest();
+var seed = '1234567890abcdefghij1234567890ab',
+    hash = crypto.createHash('sha256').update(seed).digest();
 
 // 32byte keypair lengths
 var keypair = ecc.MakeKeypair(hash);
 
 // 64byte signature lengths
-var signature = ecc.Sign(msg, keypair).toString();
-var publicKey = keypair.publicKey.toString();
-var postBody = 'sig=' + signature + '&key=' + publicKey;
-
-var body = new Buffer(postBody);
+var signature = ecc.Sign(msg, keypair).toString(),
+    publicKey = keypair.publicKey.toString(),
+    postBody = signature + '\n' + publicKey;
 
 var options = {
   url: 'http://localhost:8080/sqrl?' + nonce,
