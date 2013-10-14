cda4f2c51e9914330260f82ffc09debd3c458ee1
Brian Pinkney <bushxnyc@gmail.com>
2013-10-14 16:37:45 -0400

    Changed indents to 4 spaces

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express'),
     ecc = require('ed25519'),
     fs = require('fs'),
     url = require('url'),
+    base64url = require('base64url'),
     querystring = require('querystring');
 
 var hostname = 'localhost';
@@ -37,55 +38,48 @@ app.use(express.static(__dirname + '/static'));
 // digest the hash as the nonce and append it to the sqrl url
 // pass the string to our renderer
 app.get('/', function (req, res) {
-  var hash = crypto.createHash('md5');
-  hash.update(new Date().getTime().toString() + counter, 'utf8');
-  var nonce = hash.digest('hex');
-  var string = 'sqrl://localhost:8080/sqrl?nut=' + nonce.toString();
-  res.render('index', { url: string });
-  counter += 1;
-  urlNonce[nonce] = new Date().getTime();
-  console.log('Generated nonce: ' + nonce +
-    ' with timestamp: ' + urlNonce[nonce]);
+    var hash = crypto.createHash('md5');
+    hash.update(new Date().getTime().toString() + counter, 'utf8');
+    var nonce = hash.digest('hex');
+    var string = 'sqrl://localhost:8080/sqrl?nut=' + nonce.toString();
+    res.render('index', { url: string });
+    counter += 1;
+    urlNonce[nonce] = new Date().getTime();
+    console.log('Generated nonce: ' + nonce +
+        ' with timestamp: ' + urlNonce[nonce]);
 });
 
 // a post to our sqrl auth url
 app.post('/sqrl', function (req, res) {
-  var url_parts = url.parse(req.url, true);
-  var nonce = url_parts.query['nut'];
-  console.log('Challenge for: ' + nonce);
     var url_parts = url.parse(req.url, true);
     var nonce = url_parts.query['nut'];
     console.log('Challenge for: ' + nonce);
 
-  if (urlNonce[nonce]) {
-    var challenge = new Buffer('localhost:8080' + req.url);
-    var signature = new Buffer(req.body['sqrlsig']);
-    var key = new Buffer(url_parts.query['sqrlkey']);
+    if (urlNonce[nonce]) {
 
-    console.log("Key: " + key + ", \nSig: " + signature + ", \nChallenge: " + challenge);
         var challenge = new Buffer('localhost:8080' + req.url);
         var signature = new Buffer(base64url.toBuffer(req.body['sqrlsig'] + "="));
         var key = new Buffer(base64url.toBuffer(url_parts.query['sqrlkey'] + "=="));
 
-    try {
-      if(ecc.Verify(challenge, signature, key)) {
-        res.send(200);
-      } else {
         console.log("Key: " + key + ", \nSig: " + req.body['sqrlsig'] + ", \nChallenge: " + challenge);
 
+        try {
+            if(ecc.Verify(challenge, signature, key)) {
+                res.send(200);
+            } else {
+                res.send(400);
+            }
+        } catch (err) {
+            fs.writeFile('ECCerror.error', err, function (err) {
+                if (err) throw err;
+                console.log('Caught Err');
+            });
+            res.send(500);
+        }
+        //delete urlNonce[nonce];
+    } else {
         res.send(400);
-      }
-    } catch (err) {
-       fs.writeFile('ECCerror.error', err, function (err) {
-         if (err) throw err;
-         console.log('Caught Err');
-       });
-       res.send(500);
     }
-    delete urlNonce[nonce];
-  } else {
-    res.send(400);
-  }
 });
 
 // listen on port 8080
